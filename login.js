7561731006d56cbf26bbf10fce1fb652d5d06db6
sharvarihupare-369 <sharvarihupare15@gmail.com>
2022-11-29 22:29:28 +0530

    Login completed

--- login.js
+++ login.js
@@ -1,2 +1,12 @@
 let reg_email="rohan@gmail.com"
 let reg_pass="123@"
+
+let enter_email = "rohan@gmail.com";
+let enter_pass = "123@";
+
+if(reg_email == enter_email && reg_pass == enter_pass){
+  console.log("Login Successful");
+ }
+else{
+  console.log("Wrong Credentials");
+}
